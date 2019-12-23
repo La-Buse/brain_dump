1ed6d7c7e6bbe83f7e2dd3b6c10d2a4d40c20904
Olivier Levasseur <levasseur.olivier@courrier.uqam.ca>
2019-12-22 21:41:35 -0500

    first commit

---
+++ lib/project.dart
@@ -0,0 +1,10 @@
+import './project_interface.dart';
+class Project implements ProjectInterface {
+  String _name;
+  Project(String name) {
+    this._name = name;
+  }
+  String getName() {
+    return _name;
+  }
+}
---
+++ lib/project_interface.dart
@@ -0,0 +1,3 @@
+abstract class ProjectInterface {
+  String getName();
+}
