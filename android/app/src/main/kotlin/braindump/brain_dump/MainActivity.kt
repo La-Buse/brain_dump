a080be942344b0b042b3a85da9c874e159ff1a8d
la-buse <levasseuro.ol@gmail.com>
2020-05-24 16:24:04 -0400

    Fix crash upon application start problem

---
+++ android/app/src/main/kotlin/braindump/brain_dump/MainActivity.kt
@@ -0,0 +1,6 @@
+package braindump.brain_dump
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,17 +4,15 @@
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:reflectable/reflectable.dart';
-import 'package:brain_dump/models/calendar/calendar_item.dart';
 
-import 'package:brain_dump/widgets/my_app.dart';
+import 'package:brain_dump/main.dart';
+
 void main() {
-  testWidgets('Check that model classes implement required db functions', (WidgetTester tester) async {
-//    var reflectedClass = CalendarItem.reflectClass();
-    // Build our app and trigger a frame.
-
+//  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+//    // Build our app and trigger a frame.
 //    await tester.pumpWidget(MyApp());
 //
 //    // Verify that our counter starts at 0.
@@ -28,5 +26,5 @@ void main() {
 //    // Verify that our counter has incremented.
 //    expect(find.text('0'), findsNothing);
 //    expect(find.text('1'), findsOneWidget);
-  });
+//  });
 }
